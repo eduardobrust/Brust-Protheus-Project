84b550a22a937a7e162a18c7e41151ab6ead1366
Eduardo Brust <eduardobrust@gmail.com>
2023-10-24 17:10:55 -0300

    :sparkles: feat: consultas.sql

--- src/sql/consultas.sql
+++ src/sql/consultas.sql
@@ -1,1 +1,50 @@
-select * from sigagms.sb1000;
+SELECT B1_COD,B1_DESC,SB1.* 
+FROM SIGAGMS.SB1000 SB1
+WHERE D_E_L_E_T_ = ' '
+AND B1_FILIAL = ' '
+AND B1_MSBLQL = '2';
+
+SELECT B1_GRUPO,B1_TIPO,COUNT(*) 
+FROM SIGAGMS.SB1000 SB1
+WHERE D_E_L_E_T_ = ' '
+AND B1_FILIAL = ' '
+AND B1_MSBLQL = '2'
+GROUP BY B1_GRUPO,B1_TIPO
+ORDER BY 3 DESC;
+
+SELECT B1_COD, B1_DESC, COUNT(*) OVER () AS total_registros
+FROM (
+  SELECT B1_COD, B1_DESC
+  FROM SIGAGMS.SB1000
+  WHERE D_E_L_E_T_ = ' '
+  AND B1_FILIAL = ' '
+  AND B1_MSBLQL = '2'
+  ORDER BY B1_COD
+)
+OFFSET (1 - 1) * 10 ROWS FETCH NEXT 10 ROWS ONLY;
+--OFFSET (page - 1) * size ROWS FETCH NEXT size ROWS ONLY;
+
+----------------------------------------------------------------------
+
+SELECT B1_COD, B1_DESC
+FROM SIGAGMS.SB1000
+  WHERE D_E_L_E_T_ = ' '
+  AND B1_FILIAL = ' '
+  AND B1_MSBLQL = '2'
+ORDER BY B1_COD
+OFFSET (2000 - 1) * 10 ROWS FETCH NEXT 10 ROWS ONLY;
+
+-----------------------------------------------------------------------
+SELECT B1_COD, B1_DESC, total_registros
+FROM (
+  SELECT B1_COD, B1_DESC, ROWNUM AS rn, total_registros
+  FROM (
+    SELECT B1_COD, B1_DESC, COUNT(*) OVER () AS total_registros
+    FROM SIGAGMS.SB1000
+    WHERE D_E_L_E_T_ = ' '
+    AND B1_FILIAL = ' '
+    AND B1_MSBLQL = '2'
+    ORDER BY B1_COD
+  )
+)
+WHERE rn BETWEEN 1 AND 10;
