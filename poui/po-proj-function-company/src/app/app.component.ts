5b2bfaffdb97d9cffb23607d63225004f6160c4b
Eduardo Brust <eduardobrust@gmail.com>
2023-11-02 20:03:33 -0300

    :sparkles: feat: poui\po-proj-routine-company

--- poui/po-proj-function-company/src/app/app.component.ts
+++ poui/po-proj-function-company/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { PoMenuItem } from '@po-ui/ng-components';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
