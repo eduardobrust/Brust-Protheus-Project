63863ca7fed2e04a805e0eb862d356c2ee69532b
Eduardo Brust <eduardobrust@gmail.com>
2023-11-02 21:25:21 -0300

    :sparkles: feat: poui\po-proj-routine-company

--- poui/po-proj-function-company/src/main.ts
+++ poui/po-proj-function-company/src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import './styles.css';
+//import './styles.css';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
