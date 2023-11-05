7dc0e2bbad99e34635d175463d82e754ca4b3a34
Eduardo Brust <eduardobrust@gmail.com>
2023-11-05 15:36:02 -0300

    :sparkles: feat: poui\po-function-company - add component dynamic table

---
+++ poui/po-function-company/src/app/controllers/controllers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    DynamicTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ControllersModule { }
---
+++ poui/po-function-company/src/app/controllers/dynamic-table/dynamic-table.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DynamicTableComponent } from './dynamic-table.component';
+
+describe('DynamicTableComponent', () => {
+  let component: DynamicTableComponent;
+  let fixture: ComponentFixture<DynamicTableComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DynamicTableComponent]
+    });
+    fixture = TestBed.createComponent(DynamicTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
