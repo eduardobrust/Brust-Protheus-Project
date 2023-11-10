43405729c6c11c9b8680276cb0defceb0cd18a55
Eduardo Brust <eduardobrust@gmail.com>
2023-11-10 13:52:54 -0300

    :sparkles: feat: poui\po-function-company

--- poui/po-function-company/src/app/controllers/table-transport/po-modal-basic.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sample-po-modal-basic',
-  templateUrl: './po-modal-basic.component.html'
-})
-export class PoModalBasicComponent {}
