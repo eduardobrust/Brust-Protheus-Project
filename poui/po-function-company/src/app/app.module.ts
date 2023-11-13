26ff3fc53231126512ef42790b6fe12abed6a894
Eduardo Brust <eduardobrust@gmail.com>
2023-11-13 17:05:52 -0300

    :sparkles: feat: poui\po-function-company

--- poui/po-function-company/src/app/app.module.ts
+++ poui/po-function-company/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PoDynamicModule, PoFieldModule, PoModalModule, PoModule, PoPageModule, PoTableModule } from '@po-ui/ng-components';
 import { PoPageDynamicEditModule, PoPageDynamicSearchModule, PoPageDynamicTableModule, PoTemplatesModule } from '@po-ui/ng-templates';
-import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DynamicTableComponent } from './controllers/dynamic-table/dynamic-table.component';
@@ -16,7 +15,6 @@ import { HomeComponent } from './controllers/home/home.component';
 import { TableBasicComponent } from './controllers/table-basic/table-basic.component';
 import { TableTransportComponent } from './controllers/table-transport/table-transport.component';
 import { Router } from '@angular/router';
-//import { ProAppConfigService } from '@totvs/protheus-lib-core';
 
 @NgModule({
 declarations: [
@@ -44,8 +42,7 @@ PoDynamicModule,
 PoModalModule,
 PoPageDynamicEditModule,
 FormsModule,
-ReactiveFormsModule,
-//ProtheusLibCoreModule,
+ReactiveFormsModule
 ],
 providers: [
   {
