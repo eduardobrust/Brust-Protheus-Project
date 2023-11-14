4fb84c97ba227795ce4892839e98aced151b7aea
Eduardo Brust <eduardobrust@gmail.com>
2023-11-14 11:12:49 -0300

    :sparkles: feat: poui\po-function-company

--- poui/po-function-company/src/app/app.component.ts
+++ poui/po-function-company/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { PoMenuItem } from '@po-ui/ng-components';
-import { ProAppConfigService } from '@totvs/protheus-lib-core/services';
+import { ProAppConfigService } from '@totvs/protheus-lib-core';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +11,7 @@ export class AppComponent {
   
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', link: '/controllers/home', icon: "po-icon po-icon-home"  },
-  //  { label: 'Manager - Function x Company', link: '/controllers/dynamic-table' },
-  //  { label: 'Manager2 - Dynamic-users', link: '/controllers/dynamic-table2' },
-  //  { label: 'Manager3 - Dynamic-Brust', link: '/controllers/dynamic-table3' },
-  //  { label: 'Rotina x Empresa', link: '/controllers/table-transport',icon: "po-icon po-icon-company" },
-  //  { label: 'Rotina x Empresa - (basic)', link: '/controllers/table-basic',icon: "po-icon po-icon-company" }
+    { label: 'Rotina x Empresa', link: '/controllers/table-transport',icon: "po-icon po-icon-company" },
     { label: 'Exit',action: this.closeApp.bind(this),icon: 'po-icon po-icon-exit' }
   ];
   constructor(
