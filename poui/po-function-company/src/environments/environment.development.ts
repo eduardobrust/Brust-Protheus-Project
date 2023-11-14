74309f6635fc9e48a50abed7a17ccb3db81887f1
Eduardo Brust <eduardobrust@gmail.com>
2023-11-14 17:26:11 -0300

    :sparkles: feat: poui\po-function-company

--- poui/po-function-company/src/environments/environment.development.ts
+++ poui/po-function-company/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ import { Buffer } from 'buffer'
 export const environment = {
     production: false,
     api_baseUrl: 'http://localhost:8003/rest',
-    token: `Basic ${Buffer.from('admin:admin', 'utf8').toString('base64')}`
+    token: `Basic ${Buffer.from('admin:', 'utf8').toString('base64')}`
 };
