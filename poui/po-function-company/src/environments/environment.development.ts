128a68ace8db4a7cc0dda5e276c837ff1b413a54
Eduardo Brust <eduardobrust@gmail.com>
2023-11-23 11:03:25 -0300

    :sparkles: feat: poui\po-function-company

--- poui/po-function-company/src/environments/environment.development.ts
+++ poui/po-function-company/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
-import { Buffer } from 'buffer'
 export const environment = {
     production: false,
     api_baseUrl: '/',
-    token: `Basic ${Buffer.from('admin:', 'utf8').toString('base64')}`
+    token: ''
 };
--- poui/po-function-company/src/environments/environment.ts
+++ poui/po-function-company/src/environments/environment.ts
@@ -2,5 +2,5 @@ import { Buffer } from 'buffer';
 export const environment = {
   production: true,
   api_baseUrl: '/',
-  token: `Basic ${Buffer.from('admin:', 'utf8').toString('base64')}`
+  token: ''
 };
