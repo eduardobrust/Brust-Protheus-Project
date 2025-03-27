f2f5ef137ee2b221e57bcc840b03e07ec009b4b2
Eduardo Brust <eduardobrust@gmail.com>
2025-03-26 21:19:03 -0300

    ✨ feat: poui\po-function-company - UPDATE ANGULAR 18

--- poui/po-function-company/src/app/app.module.ts
+++ poui/po-function-company/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PoDynamicModule, PoFieldModule, PoModalModule, PoModule, PoPageModule, PoTableModule } from '@po-ui/ng-components';
@@ -12,30 +12,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './controllers/home/home.component';
 import { TableTransportComponent } from './controllers/table-transport/table-transport.component';
 
-@NgModule({
-declarations: [
-AppComponent,
-HomeComponent,
-TableTransportComponent,
-],
-imports: [
-BrowserModule,
-AppRoutingModule,
-PoModule,
-HttpClientModule,
-RouterModule.forRoot([]),
-PoTemplatesModule,
-PoTableModule,
-PoPageModule,
-PoFieldModule,
-PoDynamicModule,
-PoModalModule,
-PoPageDynamicEditModule,
-FormsModule,
-ReactiveFormsModule,
-//ProtheusLibCoreModule
-],
-providers: [],
-bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        TableTransportComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        PoModule,
+        RouterModule.forRoot([]),
+        PoTemplatesModule,
+        PoTableModule,
+        PoPageModule,
+        PoFieldModule,
+        PoDynamicModule,
+        PoModalModule,
+        PoPageDynamicEditModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
