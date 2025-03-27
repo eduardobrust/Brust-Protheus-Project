112440b6b89df44ef1ca99f7a9713d13a3fe9309
Eduardo Brust <eduardobrust@gmail.com>
2025-03-26 22:10:44 -0300

    ✨ feat: poui\po-function-company - UPDATE protheus-lib-core@19

--- poui/po-function-company/src/app/app.module.ts
+++ poui/po-function-company/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { PoDynamicModule, PoFieldModule, PoModalModule, PoModule, PoPageModule, PoTableModule } from '@po-ui/ng-components';
+import { PoDynamicModule, PoFieldModule, PoModalModule, PoModule, PoPageModule, PoTableModule, PoI18nModule, PoI18nConfig } from '@po-ui/ng-components';
 import { PoPageDynamicEditModule, PoTemplatesModule } from '@po-ui/ng-templates';
 import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,22 +12,62 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './controllers/home/home.component';
 import { TableTransportComponent } from './controllers/table-transport/table-transport.component';
 
-@NgModule({ declarations: [
-        AppComponent,
-        HomeComponent,
-        TableTransportComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        PoModule,
-        RouterModule.forRoot([]),
-        PoTemplatesModule,
-        PoTableModule,
-        PoPageModule,
-        PoFieldModule,
-        PoDynamicModule,
-        PoModalModule,
-        PoPageDynamicEditModule,
-        FormsModule,
-        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+// Defina o InjectionToken para I18N_CONFIG
+const I18N_CONFIG = new InjectionToken<PoI18nConfig>('I18N_CONFIG');
+
+// Defina os literais para os idiomas suportados
+const i18nLiterals = {
+  pt: {
+    welcome: 'Bem-vindo',
+    // Adicione outros literais conforme necessário
+  },
+  en: {
+    welcome: 'Welcome',
+    // Adicione outros literais conforme necessário
+  },
+  es: {
+    welcome: 'Bienvenido',
+    // Adicione outros literais conforme necessário
+  }
+};
+
+// Configuração do PoI18n
+const i18nConfig: PoI18nConfig = {
+  default: {
+    language: 'pt', // Idioma padrão
+    context: 'general', // Contexto padrão
+  },
+  contexts: {
+    general: i18nLiterals // Literais para o contexto 'general'
+  }
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TableTransportComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule,
+    PoTableModule,
+    PoPageModule,
+    PoFieldModule,
+    PoDynamicModule,
+    PoModalModule,
+    PoPageDynamicEditModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PoI18nModule // Adiciona o PoI18nModule sem forRoot
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: I18N_CONFIG, useValue: i18nConfig } // Fornece a configuração diretamente via providers
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
